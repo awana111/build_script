a7173004e4c6ce62a1f075c3cc4cf75d0956350c
awana <80144987+awana111@users.noreply.github.com>
2025-07-03 00:48:56 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 #repo init
-repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone https://github.com/awana111/local_manifests.git -b Rising .repo/local_manifests
+git clone https://github.com/ikwfahmi/local_manifests.git -b matrixx .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -29,4 +29,4 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-breakfast X00TD userdebug && make installclean && rise b
+brunch X00TD
